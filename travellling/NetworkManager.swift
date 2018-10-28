833ba812cf4c904f39c0d8cf8209b8893b49f1a7
Santosh  Kumar Chaurasia <santoshchaurasia@Anurags-MacBook-Air.local>
2018-10-29 02:30:37 +0530

    second commit

--- travellling/NetworkManager.swift
+++ travellling/NetworkManager.swift
@@ -14,59 +14,42 @@ class NetworkManager: NSObject {
     static let shared = NetworkManager()
     var imagesResult : [flickerPhotoData]?
     let path = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=3e7cc266ae2b0e0d78e279ce8e361736&format=json&nojsoncallback=1&safe_search=1&text="
-    var pages = 0
+    
     override init()
     {
         
     }
     
-    func sendRequestForImagesData(searchText:String, complete: @escaping (Any?,String)->(Void)){
+    func sendRequestForImagesData(searchText:String, complete: @escaping (Any?)->(Void)){
         
         let urlRequest = self.path + searchText
         guard let url = URL(string: urlRequest) else {
-            complete(nil,"Url_Error")
             return
         }
-        
-        URLSession.shared.dataTask(with: url, completionHandler :{ [weak self ] (data , response ,error) in
-            guard let data = data else {
-                complete(nil,"wrongResponse")
-                return
+        URLSession.shared.dataTask(with: url, completionHandler :{ (data , response ,error) in
+            if let error = error {
+                complete(error)
             }
-            let jsonData = try? JSONSerialization.jsonObject(with: data, options:[]) as! NSDictionary
-            let status = jsonData?.value(forKey: "stat") as? String
-            if status == "ok" {
-                let photos = jsonData?.value(forKey: "photos") as? NSDictionary
-                let photo = photos?.value(forKey: "photo") //as! [[String : Any]]
-                self?.pages = photos?.value(forKey: "pages") as! Int
-//                for imageData in photo {
-//                    if let id = imageData["id"] as? String,
-//                        let secret = imageData["secret"] as? String,
-//                        let farm = imageData["farm"] as? String,
-//                        let server = imageData["server"] as? String {
-//                        self?.imagesResult?.append(flickerPhotoData(id:id,secret:secret,farm:farm,server:server))
-//                    }
-//                }
-                complete(photo,status ?? "ok")
+            guard let data = data,
+                let jsonData = try? JSONSerialization.jsonObject(with: data, options:[]) as! NSDictionary,
+                let status = jsonData.value(forKey: "stat") as? String ,
+                let photos = jsonData.value(forKey: "photos") as? NSDictionary,
+                let photo = photos.value(forKey: "photo") else {
+                    return
             }
-            else {
-                complete(nil,status!)
+            if( status == "ok") {
+                complete(photo)
+            } else {
+                complete(nil)
             }
         }).resume()
     }
-    
-    func getImageFor(id:String,secret:String,farm:String,server:String, complete :@escaping (UIImage) -> Void )
-    {
-        
-        
-    }
-    
 }
 
 enum flickerApi  {
     //   "http://farm{farm}.static.flickr.com/{server}/{id}_{secret}.jpg"
     static let imagePath = "http://farm%@.static.flickr.com/%@/%@_%@.jpg"
-    case getFLickerImagePathFor(farm:Int,server:String,id:String,secret:String)
+    case getFLickerImagePathFor(farm:String,server:String,id:String,secret:String)
     
     var path : String {
         switch self {
